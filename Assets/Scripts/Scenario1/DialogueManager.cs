634efd98c77aee095234ed2331ed678da65642c5
Fatima Nadeem <31242975+fat1nad@users.noreply.github.com>
2021-03-19 13:43:43 +0500

    Working on dialogue system

---
+++ Assets/Scripts/Scenario1/DialogueManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    static public DialogueManager instance; 
+    private Queue<string> sentences;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            sentences = new Queue<string>();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) // For now its space
+        {
+            DisplayNextSentence();
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting Monologue");
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    public void EndDialogue()
+    {
+        Debug.Log("End of Monologue");
+    }
+
+
+}
