ebcbae58b23bd52c216f441b775c2dee43ed069f
Muhammad Shahrom Ali <estineali@gmail.com>
2021-03-19 21:30:25 +0500

    Imported Cinemachine, Player movement script renamed with Scenario 2 Naming Convention -> Prefix S2

---
+++ Assets/Scripts/Scenario2/S2PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+
+public class S2PlayerMovement : MonoBehaviour
+{
+    float movementSpeed = 5f;
+    Rigidbody2D rb;
+    Animator animator; 
+
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        movement = Vector2.zero;
+    }
+
+
+    private void Update()
+    {
+        //Handle Input here 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    //Called a fixed amount of times every second.
+    void FixedUpdate()
+    {
+        //Handle Movement here
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime );
+    }
+}
