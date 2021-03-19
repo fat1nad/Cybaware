cdabd6e9d6fb231d942c86da597fb963eddda1af
Muhammad Shahrom Ali <estineali@gmail.com>
2021-03-19 09:53:30 +0500

    Scenario 2: Player Animated

--- Assets/Scripts/Scenario2/PlayerMovement.cs
+++ Assets/Scripts/Scenario2/PlayerMovement.cs
@@ -7,12 +7,14 @@ public class PlayerMovement : MonoBehaviour
 {
     float movementSpeed = 5f;
     Rigidbody2D rb;
+    Animator animator; 
 
     Vector2 movement;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
         movement = Vector2.zero;
     }
 
@@ -22,6 +24,12 @@ public class PlayerMovement : MonoBehaviour
         //Handle Input here 
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
     }
 
     //Called a fixed amount of times every second.
