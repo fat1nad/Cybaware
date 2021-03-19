d2aaad0514829705f2e09c8df870cc1f5d4e7a9c
Muhammad Shahrom Ali <estineali@gmail.com>
2021-03-19 09:37:10 +0500

    Scenario 2: Player movement

---
+++ Assets/Scripts/Scenario2/PlayerMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerMovement : MonoBehaviour
+{
+    float movementSpeed = 5f;
+    Rigidbody2D rb;
+
+    Vector2 movement;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        movement = Vector2.zero;
+    }
+
+
+    private void Update()
+    {
+        //Handle Input here 
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    //Called a fixed amount of times every second.
+    void FixedUpdate()
+    {
+        //Handle Movement here
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime );
+    }
+}
