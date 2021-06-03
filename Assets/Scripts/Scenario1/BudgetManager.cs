380431c6238e5ed92c7d8efe378d69a3e3df8d77
Fatima Nadeem <31242975+fat1nad@users.noreply.github.com>
2021-06-03 20:32:06 +0500

    added more to the sofa shopping part

---
+++ Assets/Scripts/Scenario1/BudgetManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BudgetManager : MonoBehaviour
+{
+    public Text budgetText;
+    public DialogueManager dialogueManager;
+    public Dialogue penaltyDialogue;
+
+    private int budget;
+    
+    void Start()
+    {
+        budget = 10000;
+        budgetText.text = "Budget: " + budget.ToString(); // displaying
+                                                          // budget
+    }
+
+    public void Penalize(int penalty)
+    {
+        StartCoroutine(DelayedPenalize(penalty));
+    }
+
+    public void Buy(int price)
+    {
+        Debug.Log("buying");
+
+        budget -= price;
+        budgetText.text = "Budget: " + budget.ToString(); // displaying
+                                                          // budget
+    }
+
+    IEnumerator DelayedPenalize(int penalty)
+    {
+        Debug.Log("penalizing");
+        
+        yield return new WaitForSeconds(3);
+
+        budget -= penalty;
+        budgetText.text = "Budget: " + budget.ToString(); // displaying
+                                                          // budget
+
+        dialogueManager.StartDialogue(penaltyDialogue);
+    }
+}
